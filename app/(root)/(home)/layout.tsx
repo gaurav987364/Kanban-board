a44526bcb9fbb0b8ebe919c7a44c545450f86a67
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-01-19 15:29:42 +0530

    ...cleaning

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -19,4 +19,4 @@ const layout = ({children}:{children:React.ReactNode}) => {
   )
 }
 
-export default layout
+export default layout;
--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -26,4 +26,4 @@ const LeftSideBar = () => {
   )
 }
 
-export default LeftSideBar
+export default LeftSideBar;
