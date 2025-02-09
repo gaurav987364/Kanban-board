540a23505c2fe83ec4351e55f8dc4aaeb8e4d4c6
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-02-09 19:11:37 +0530

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "●●BoardUp | Manage your tasks.",
+  title: "●●BoardUp | Manage your tasks!",
   description: "A kanban board is one of the tools that can be used to implement kanban to manage work at a personal or organizational level.Kanban boards visually depict work at various stages of a process using cards to represent work items and columns to represent each stage of the process. ",
 };
 
